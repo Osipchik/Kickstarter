1d385d13b96b2ab18286c482c5bf382b25e5ab59
Tim <WyTrapT@outlook.com>
2020-04-28 16:03:12 +0300

    feature: Event bus

---
+++ Messages/CategoryDeletingMessage.cs
@@ -0,0 +1,14 @@
+﻿namespace Messages
+{
+    public class CategoryDeletingMessage
+    {
+        public int Id;
+        public int NewId;
+        
+        public CategoryDeletingMessage(int id, int newId)
+        {
+            Id = id;
+            NewId = newId;
+        }
+    }
+}
---
+++ Messages/CompanyLaunching.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Messages
+{
+    public class CompanyLaunching
+    {
+        public string CompanyId;
+        public DateTime EndDate;
+        public float Goal;
+
+        public CompanyLaunching(string companyId, DateTime endDate, float goal)
+        {
+            CompanyId = companyId;
+            EndDate = endDate;
+            Goal = goal;
+        }
+    }
+}
