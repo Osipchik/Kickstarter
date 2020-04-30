31753c90c99d2f0d2a31e98c3ddaffd009e1e6e7
Tim <WyTrapT@outlook.com>
2020-04-30 18:12:33 +0300

    feature: mail message

---
+++ Messages/MailMessage.cs
@@ -0,0 +1,16 @@
+﻿namespace Messages
+{
+    public class MailMessage
+    {
+        public string To { get; set; }
+        public string Name { get; set; }
+        public string CallbackUrl { get; set; }
+
+        public MailMessage(string to, string name,  string callbackUrl)
+        {
+            To = to;
+            Name = name;
+            CallbackUrl = callbackUrl;
+        }
+    }
+}
