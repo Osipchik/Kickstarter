2be51ac532460a9c1734d69987f3471cd089e1d0
Tim <WyTrapT@outlook.com>
2020-06-10 14:35:20 +0300

    feature: ocelot gateway

---
+++ ApiGateway/Program.cs
@@ -0,0 +1,39 @@
+using System.IO;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ApiGateway
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            var builder = WebHost.CreateDefaultBuilder(args);
+
+            builder.ConfigureServices(s => s.AddSingleton(builder))
+                .ConfigureAppConfiguration(
+                    ic => ic.AddJsonFile(Path.Combine("configuration",
+                        "configuration.json")))
+                .UseStartup<Startup>();
+            var host = builder.Build();
+            return host;
+        }
+        
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); })
+                .ConfigureAppConfiguration((hostContext, config) =>
+                {
+                    config.AddJsonFile("ocelot.json");
+                });
+    }
+}
