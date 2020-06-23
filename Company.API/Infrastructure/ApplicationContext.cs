03b9e283207c098250f6f7d22defcbde93ef7f98
Tim <WyTrapT@outlook.com>
2020-06-23 14:39:36 +0300

    fix: remove extra modelBuilder

--- Company.API/Infrastructure/ApplicationContext.cs
+++ Company.API/Infrastructure/ApplicationContext.cs
@@ -5,42 +5,18 @@ namespace Company.API.Infrastructure
 {
     public class ApplicationContext : DbContext
     {
-        public DbSet<CompanyItem> CompanyContext { get; set; }
+        public DbSet<CompanyStory> StoryContext { get; set; }
         public DbSet<CompanyPreview> PreviewContext { get; set; }
-        public DbSet<CompanyFunding> FundingContext { get; set; }
-        // public DbSet<CompanyReward> RewardContext { get; set; }
 
         public ApplicationContext(DbContextOptions options) : base(options) { }
         
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<CompanyPreview>()
-                .HasOne(i => i.CompanyItem)
-                .WithOne(p => p.CompanyPreview)
-                .OnDelete(DeleteBehavior.Cascade);
-
-            modelBuilder.Entity<CompanyPreview>()
-                .HasOne(i => i.CompanyFunding)
-                .WithOne(p => p.CompanyPreview)
-                .OnDelete(DeleteBehavior.Cascade);
-
-            
-            modelBuilder.Entity<CompanyFunding>()
-                .HasOne(p => p.CompanyPreview)
-                .WithOne(f => f.CompanyFunding)
-                .HasForeignKey<CompanyPreview>(i => i.CompanyFundingId)
-                .OnDelete(DeleteBehavior.Cascade);
-
-            modelBuilder.Entity<CompanyItem>()
-                .HasMany(i => i.CompanyImages)
+                .HasOne<CompanyStory>()
                 .WithOne()
+                .HasForeignKey<CompanyPreview>(i => i.CompanyStoryId)
                 .OnDelete(DeleteBehavior.Cascade);
-
-            // modelBuilder.Entity<CompanyReward>()
-            //     .HasOne<CompanyItem>()
-            //     .WithMany()
-            //     .HasForeignKey(p => p.CompanyId)
-            //     .OnDelete(DeleteBehavior.Cascade);
         }
     }
 }
--- Company.API/Infrastructure/DbInit.cs
+++ Company.API/Infrastructure/DbInit.cs
@@ -1,75 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Company.API.Models;
+using EventBus.Events;
+using MassTransit;
 
 namespace Company.API.Infrastructure
 {
     public class DbInit
     {
-        public static void Initialize(ApplicationContext context)
+        public static async Task Initialize(ApplicationContext context, IBusControl bus)
         {
             //context.Database.EnsureDeleted();
             context.Database.EnsureCreated();
             
-            if (context.CompanyContext.Any())
+            if (context.StoryContext.Any())
             {
                 return;
             }
 
-            CreateCompanies(context);
-            CreatePreviews(context);
+            await CreateCompanies(context, bus);
         }
 
-        private static void CreateCompanies(ApplicationContext context)
+        private static async Task CreateCompanies(ApplicationContext context, IBusControl bus)
         {
-            var companies = new List<CompanyItem>();
-            var fundings = new List<CompanyFunding>();
+            var companies = new List<CompanyStory>();
+            var previews = new List<CompanyPreview>();
+            
+            string[] ids =
+            {
+                Guid.NewGuid().ToString(), 
+                Guid.NewGuid().ToString(), 
+                Guid.NewGuid().ToString(), 
+                Guid.NewGuid().ToString(), 
+                Guid.NewGuid().ToString(), 
+            };
+
+            var ownerId = "f10c50e5-119c-433a-a2fd-66f7bbfdac29";
             
             for (var i = 0; i < 5; i++)
             {
-                companies.Add(new CompanyItem
+                companies.Add(new CompanyStory
                 {
-                    Id = Guid.NewGuid().ToString(),
-                    OwnerId = Guid.NewGuid().ToString(),
+                    Id = ids[i],
+                    OwnerId = ownerId,
                     Story = $"story # {i}",
                     Risks = $"risks # {i}",
                     CreationDate = DateTime.Now,
                     LaunchDate = DateTime.Now
                 });
 
-                fundings.Add(new CompanyFunding
-                {
-                    Id = Guid.NewGuid().ToString(),
-                    Founded = i,
-                    Goal = i * i
-                });
-            }
-            
-            context.CompanyContext.AddRange(companies);
-            context.FundingContext.AddRange(fundings);
-            context.SaveChanges();
-        }
-
-        private static void CreatePreviews(ApplicationContext context)
-        {
-            var companies = context.CompanyContext.ToList();
-            var fundings = context.FundingContext.ToList();
-            
-            var previews = new List<CompanyPreview>();
-
-            for (var i = 0; i < 5; i++)
-            {
                 previews.Add(new CompanyPreview
                 {
-                    Id = Guid.NewGuid().ToString(),
+                    Id = ids[i],
+                    OwnerId = ownerId,
                     Description = $"preview: {i}",
                     Title = $"company: {i}",
-                    CompanyItemId = companies[i].Id,
-                    CompanyFundingId = fundings[i].Id
+                    CompanyStoryId = companies[i].Id,
+                    CreationDate = DateTime.Now
+                });
+                
+                await bus.Publish<ICompanyCreated>(new
+                {
+                    CompanyId = ids[i],
+                    UserId = ownerId
                 });
             }
 
+            context.StoryContext.AddRange(companies);
             context.PreviewContext.AddRange(previews);
             context.SaveChanges();
         }
